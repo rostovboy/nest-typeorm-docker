44af6d284327ac56fa7472740d82e1deaae4c87e
Alexey T <rostovboy@gmail.com>
2023-05-23 23:10:38 +0300

    Generated Res, Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const config = new DocumentBuilder()
+    .setTitle('Nest API Example')
+    .setDescription('The API Description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+  app.enableCors(); // https://docs.nestjs.com/security/cors
   const PORT = configService.get<number>('PORT') || 3000;
   await app.listen(PORT);
 }
